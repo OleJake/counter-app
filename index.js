9ed7c7043b3e9feaa5485c6afd523202baf68747
OleJake <jacobshawn8@outlook.com>
2022-10-12 22:44:42 -0400

    finish save counter
    finish save counter

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+console.log(saveEl)
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
     console.log(count)
 }
